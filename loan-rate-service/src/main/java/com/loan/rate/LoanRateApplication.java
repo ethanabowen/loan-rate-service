a590af311ad744bcf541f5d39de43cc10fe35bd5
Ethan <Ethan@Ethan-PC.home>
2017-07-29 21:45:07 -0500

    guava json mapping working

--- loan-rate-service/src/main/java/com/loan/rate/LoanRateApplication.java
+++ loan-rate-service/src/main/java/com/loan/rate/LoanRateApplication.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+
 @SpringBootApplication
 @Profile("app")
 @ComponentScan("com.loan")
@@ -28,6 +31,18 @@ public class LoanRateApplication {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+  
+//  @Bean
+//  ObjectMapper customizeJacksonConfiguration() {
+//      ObjectMapper om = new ObjectMapper();
+//      om.registerModule(new GuavaModule());
+//      return om;
+//  }
+//  
+  @Bean
+  public Module guavaModule() {
+      return new GuavaModule();
   }
 
 }
